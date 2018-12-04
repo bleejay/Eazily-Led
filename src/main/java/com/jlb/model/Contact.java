6774a39958b991c3be771b5435bd817b34bab2af
Jason <jasonblee@aol.com>
2018-12-04 10:47:21 +0000

    working email sender from form input values

--- src/main/java/com/jlb/model/Contact.java
+++ src/main/java/com/jlb/model/Contact.java
@@ -7,45 +7,52 @@ public class Contact {
     private String email;
     private String message;
 
-
-    public Contact(){}
+    public Contact(String name, String subject, String contactNumber, String email, String message) {
+        this.name = name;
+        this.subject = subject;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        this.message = message;
+    }
 
 
     public String getName() {
         return name;
-    }
-    public String message(){
-        return message;
-    }
-    public void setMessage(){
-        this.message = message;
-    }
-    public String getSubject() {
-        return subject;
-    }
-
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getSubject() {
+        return subject;
+    }
+
     public void setSubject(String subject) {
         this.subject = subject;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
     }
 
     public void setContactNumber(String contactNumber) {
         this.contactNumber = contactNumber;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
